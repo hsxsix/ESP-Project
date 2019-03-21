2c550a639381a322aaaaf44a69ccb2111964ff43
hsxsix <hsxsix@gmail.com>
2019-03-21 18:05:30 +0800

    增加天气显示

--- old_tv/otv.py
+++ old_tv/otv.py
@@ -18,14 +18,18 @@ class OTV():
     def __init__(self):
         spi = SPI(2, sck=Pin(18), mosi=Pin(23), miso=Pin(19),baudrate=14500000)
         self.display = Display(spi, dc=Pin(17), cs=Pin(5), rst=Pin(16))
+        # self.display("连接MQTT服务器...")
         self.mqtt = network.mqtt("otv", "mqtt://t.hsxsix.com", user="six", password="1234$#@!", 
                         cleansession=True, connected_cb=self.conncb, disconnected_cb=self.disconncb, 
                         subscribed_cb=self.subscb, published_cb=self.pubcb, data_cb=self.datacb)
+        
+        self.weather_api = 'http://118.24.144.127/weather/v1?city={}'
 
     def publish(self, msg):
         self.mqtt.publish('otv', 'Hi from Micropython')
-
+    
     def conncb(self, task):
+        # self.dispaly("连接MQTT成功")
         print("[{}] Connected".format(task))
         self.mqtt.subscribe('otv')
 
@@ -43,18 +47,23 @@ class OTV():
         data format:
         "tv_id|image url|XX|XX|XX"
         '''
-        # print("[{}] Data arrived from topic: {}, Message:\n".format(msg[0], msg[1]), msg[2])
         _, image_url, image_size, position = msg[2].split('|')
         print("Received a task to show image:\nimage url:{},\nimage size:{}".format(image_url,image_size))
-        download_image = self.http_image(image_url)
+        # self.display.draw_image("收到文件，正在下载。。。")
+        download_image = self.http_get(image_url)
         w,h = image_size.split(',')
         x,y = position.split(',')
         if download_image:
             self.display.clear()
             self.display.draw_image(download_image, int(x), int(y), int(w), int(h))
+    
+    def show_weather(self):
+        # self.display("正在更新天气数据。。。")
+        weather_data = self.http_get(self.weather_api, types='text', file_name='weather.txt')
+        
 
     #根据图片url下载对应的图片文件
-    def http_image(self, url, image_name=None):
+    def http_get(self, url, types='image', file_name=None):
         print("start download image:{}".format(url))
         try:
             proto, dummy, host, path = url.split("/", 3)
@@ -71,8 +80,8 @@ class OTV():
             port = int(port)
         
         addr = socket.getaddrinfo(host, port)[0][-1]
-        if not image_name:
-            image_name = path.split('/')[-1]
+        if not file_name:
+            file_name = path.split('/')[-1]
         s = socket.socket()
         s.connect(addr)
         s.send(bytes('GET /%s HTTP/1.0\r\nHost: %s\r\n\r\n' % (path, host), 'utf8'))
@@ -83,15 +92,25 @@ class OTV():
                 print(data)
                 break
 
-        with open (image_name, 'wb') as f:
-            while True:
-                data = s.recv(512)
-                if data:
-                    f.write(data)
-                else:
-                    print("download done!")
-                    download = image_name
-                    break
+        if types == 'image':
+            with open (file_name, 'wb') as f:
+                while True:
+                    data = s.recv(512)
+                    if data:
+                        f.write(data)
+                    else:
+                        print("download done!")
+                        download = file_name
+                        break 
+        else:
+            with open(file_name, 'w') as f:
+                while True:
+                    data = s.recv(512)
+                    if data:
+                        f.write(data)
+                    else:
+                        download = file_name 
+                        break 
         s.close()
         return download
 
