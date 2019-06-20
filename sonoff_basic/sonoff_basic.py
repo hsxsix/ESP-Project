cd2c776123b4dbf9b8ee2390437ad183fb3070f7
hsxsix <hsxsix@gmail.com>
2019-06-20 10:23:58 +0800

    micropython program for sonoff basic

--- sonoff_basic/sonoff_basic.py
+++ sonoff_basic/sonoff_basic.py
@@ -14,10 +14,12 @@ class SonOff():
     device_status = "OFF"
 
     def __init__(self):
+        print("initialize...check PowerOnState")
         if config.POWER_ON_STATE:
-            self.relay.value(1)
+            print("PowerONState = 1, turn on relay...")
+            self.relay_control(1)
+            self.led_control(1)
             self.device_status = "ON"
-            self.led.value(1)
         self.mqtt_client = MQTTClient(config.DEVICE_NAME, config.MQTT_SERVER,
                         config.MQTT_PORT, config.MQTT_USER, config.MQTT_PASSWD)
         self.mqtt_client.set_callback(self.sub_callback)
@@ -25,46 +27,59 @@ class SonOff():
         self.ping_mqtt = 0
         self.ping_fail = 0
         self.button_interrupt = 0
+    
+    def relay_control(self, value):
+        self.relay.value(value)
+        return self.relay.value()
+
+    def led_control(self, value):
+        self.led.value(value)
+        return self.led.value()
 
     def mqtt_connect(self):
+        print("connect to mqtt server...")
         try:
             self.mqtt_client.connect()
-            self.mqtt_client.subscribe(config.STATUS_TOPIC)
-            self.mqtt_client.subscribe(config.SET_TOPIC)
-            self.mqtt_client.subscribe(config.POS_TOPIC)
-            self.mqtt_client.subscribe(config.MQTT_CHECK)
+            print("connect to mqtt server success!")
+            for topic in [config.STATE_TOPIC, config.SET_TOPIC, config.POS_STATE_TOPIC, config.POS_SET_TOPIC, config.MQTT_CHECK]:
+                self.mqtt_client.subscribe(topic)
+            self.publish_device_status()
+            self.publish_pos_status("ON" if config.POWER_ON_STATE else "OFF")
         except:
+            print("mqtt server connect failed!")
             pass
 
     def sub_callback(self, topic, msg):
         topic = topic.decode('utf-8')
         msg = msg.decode('utf-8')
+        print("recive mqtt message:{}, from {}".format(msg, topic))
         if topic == config.SET_TOPIC:
             if msg == "ON":
-                self.relay.value(1)
+                self.relay_control(1)
+                self.led_control(1)
                 self.device_status = "ON"
-                self.led.value(1)
             else:
-                self.relay.value(0)
+                self.relay_control(0)
+                self.led_control(0)
                 self.device_status = "OFF"
-                self.led.value(0)
             self.publish_device_status()
-        elif topic == config.POS_TOPIC:
+        elif topic == config.POS_SET_TOPIC:
             if msg == "ON":
                 self.change_config({"POWER_ON_STATE": 1})
             else:
                 self.change_config({"POWER_ON_STATE": 0})
+            self.publish_pos_status(msg)
         elif topic == config.MQTT_CHECK:
             if int(msg) == self.ping_mqtt:
                 self.ping_fail = 0
     
     def publish_device_status(self):
         if self.ping_mqtt == 0:
-            self.mqtt_client.publish(config.STATUS_TOPIC, self.device_status)
+            self.mqtt_client.publish(config.STATE_TOPIC, self.device_status)
     
     def publish_pos_status(self, value):
         if self.ping_mqtt == 0:
-            self.mqtt_client.publish(config.POS_TOPIC, value)
+            self.mqtt_client.publish(config.POS_STATE_TOPIC, value)
 
     def button_action(self, pin):
         self.button_interrupt = 1
@@ -105,14 +120,15 @@ class SonOff():
     async def check_button(self):
         while True:
             await asyncio.sleep(0.3)
-            if self.button_interrupt == 0:
-                pass 
-            else:
+            if self.button_interrupt > 0:
+                print("pressed the button...")
                 if self.device_status == "ON":
-                    self.relay.value(0)
+                    self.relay_control(0)
+                    self.led_control(0)
                     self.device_status = "OFF"
                 else:
-                    self.relay.value(1)
+                    self.relay_control(1)
+                    self.led_control(1)
                     self.device_status == "ON"
                 self.button_interrupt = 0
                 self.publish_device_status()
@@ -120,7 +136,6 @@ class SonOff():
     def run(self):
         self.button.irq(trigger=Pin.IRQ_FALLING, handler=self.button_action)
         self.mqtt_connect()
-        self.publish_device_status()
         loop = asyncio.get_event_loop()
         loop.create_task(self.check_message())
         loop.create_task(self.check_mqtt())
