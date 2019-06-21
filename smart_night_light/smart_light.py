8999f1f6cc3c407a6c8d53bb01f66bda0d087cb3
hsxsix <hsxsix@gmail.com>
2019-06-21 17:57:55 +0800

    fix bug

--- smart_night_light/smart_light.py
+++ smart_night_light/smart_light.py
@@ -8,7 +8,7 @@ import micropython
 import uasyncio as asyncio
 from neopixel import NeoPixel
 from umqtt.simple import MQTTClient
-from machine import Pin, Timer, WDT
+from machine import Pin, Timer, WDT, ADC
 
 import config
 from status_code import code_table
@@ -19,12 +19,12 @@ micropython.alloc_emergency_exception_buf(100)
 class SmartLight():
     def __init__(self):
         self.wdt = WDT()
+        self.adc = ADC(0)
         self.auto_tim = Timer(1)
         self.publish_tim = Timer(2)
         self.ping_fail = 0
         self.ping_mqtt = 0
         self.light_state = 0
-        self.int_err_count = 0
         self.light_intensity = 0
         self.sensor_interrupt = 0
         self.button_interrupt = 0
@@ -45,8 +45,9 @@ class SmartLight():
 
         self.np = NeoPixel(Pin(config.WS2812_PIN, Pin.OUT), config.WS2812_BIT)
 
-        self.mqtt_client = MQTTClient("color_light", config.MQTT_SERVER,
-                        config.MQTT_PORT,config.MQTT_USER, config.MQTT_PASSWD)
+        self.mqtt_client = MQTTClient(client_id=config.DEVICE_NAME, server=config.MQTT_SERVER,
+                                    port=config.MQTT_PORT, user=config.MQTT_USER, 
+                                    password=config.MQTT_PASSWD, keepalive=60)
         self.mqtt_client.set_callback(self.sub_callback)
         self.mqtt_client.set_last_will(config.AVAILABILITY_TOPIC, "offline")
 
@@ -54,19 +55,19 @@ class SmartLight():
         self.human_sensor_2 = Pin(config.HUMAN_SENSOR_PIN_2, Pin.IN, Pin.PULL_UP)
         self.human_sensor_3 = Pin(config.HUMAN_SENSOR_PIN_3, Pin.IN, Pin.PULL_UP)
         self.sound_sensor = Pin(config.SOUND_SENSOR_PIN, Pin.IN, Pin.PULL_UP)
-        self.button = Pin(config.BUTTON_PIN_1, Pin.IN, Pin.PULL_UP)
+        self.button = Pin(config.BUTTON_PIN, Pin.IN, Pin.PULL_UP)
 
     def mqtt_connect(self):
         try:
             self.mqtt_client.connect()
-            for topic in [config.STATUS_TOPIC, config.SET_TOPIC, config.MQTT_CHECK_TOPIC,
-                    config.HUMAN_SET_TOPIC, config.LIGHT_SET_TOPIC, config.SOUND_SET_TOPIC]:
+            for topic in [config.DEVICE_SET_TOPIC, config.MQTT_CHECK_TOPIC, config.HUMAN_SET_TOPIC, 
+                            config.LIGHT_SET_TOPIC, config.SOUND_SET_TOPIC]:
                 self.mqtt_client.subscribe(topic)
             self.mqtt_client.publish(config.AVAILABILITY_TOPIC, "online")
-            self.mqtt_client.publish(config.STATUS_TOPIC, json.dumps(self.light_status))
-            self.mqtt_client.publish(config.HUMAN_SET_TOPIC, self.auto_human)
-            self.mqtt_client.publish(config.LIGHT_SET_TOPIC, self.auto_light)
-            self.mqtt_client.publish(config.SOUND_SET_TOPIC, self.auto_sound)
+            self.mqtt_client.publish(config.DEVICE_STATE_TOPIC, json.dumps(self.light_status))
+            self.mqtt_client.publish(config.HUMAN_STATE_TOPIC, self.auto_human)
+            self.mqtt_client.publish(config.LIGHT_STATE_TOPIC, self.auto_light)
+            self.mqtt_client.publish(config.SOUND_STATE_TOPIC, self.auto_sound)
         except Exception as e:
             print("mqtt connect faild", e)
 
@@ -85,14 +86,14 @@ class SmartLight():
     def sub_callback(self, topic, msg):
         topic = topic.decode('utf-8')
         msg = msg.decode('utf-8')
-        if topic == config.SET_TOPIC:
+        if topic == config.DEVICE_SET_TOPIC:
             msg = json.loads(msg)
             state = msg.get('state', '')
             brightness = msg.get('brightness', '')
             color = msg.get('color', {})
             effect = msg.get('effect', '')
             self.light_control(state, brightness, color, effect)
-            self.mqtt_client.publish(config.STATUS_TOPIC, json.dumps(self.light_status))
+            self.mqtt_client.publish(config.DEVICE_STATE_TOPIC, json.dumps(self.light_status))
         elif topic == config.MQTT_CHECK_TOPIC:
             if int(msg) == self.ping_mqtt:
                 # print("MQTT is OK")
@@ -100,24 +101,24 @@ class SmartLight():
         elif topic == config.HUMAN_SET_TOPIC:
             if msg == "ON":
                 self.auto_human = 1
-                self.mqtt_client.publish(config.HUMAN_SET_TOPIC, "ON")
+                self.mqtt_client.publish(config.HUMAN_STA_TOPIC, "ON")
             elif msg == "OFF":
                 self.auto_human = 0
-                self.mqtt_client.publish(config.HUMAN_SET_TOPIC, "OFF")
+                self.mqtt_client.publish(config.HUMAN_STATE_TOPIC, "OFF")
         elif topic == config.LIGHT_SET_TOPIC:
             if msg == "ON":
                 self.auto_light = 1
-                self.mqtt_client.publish(config.LIGHT_SET_TOPIC, "ON")
+                self.mqtt_client.publish(config.LIGHT_STATE_TOPIC, "ON")
             elif msg == "OFF":
                 self.auto_light = 0
-                self.mqtt_client.publish(config.LIGHT_SET_TOPIC, "OFF")
+                self.mqtt_client.publish(config.LIGHT_STATE_TOPIC, "OFF")
         elif topic == config.SOUND_SET_TOPIC:
             if msg == "ON":
                 self.auto_sound = 1
-                self.mqtt_client.publish(config.SOUND_SET_TOPIC, "ON")
+                self.mqtt_client.publish(config.SOUND_STATE_TOPIC, "ON")
             elif msg == "OFF":
                 self.auto_sound = 0
-                self.mqtt_client.publish(config.SOUND_SET_TOPIC, "OFF")
+                self.mqtt_client.publish(config.SOUND_STATE_TOPIC, "OFF")
 
 
     def light_control(self, state, brightness, color, effect):
@@ -170,7 +171,7 @@ class SmartLight():
         self.publish_tim.init(period=1000, mode=Timer.ONE_SHOT, callback=self.publish_light_status)
 
     def get_light_intensity(self):
-        li = 540
+        li = self.adc.read()
         return 1 if li > 512 else 0
 
     def publish_light_status(self):
@@ -206,8 +207,7 @@ class SmartLight():
                     pass
                 self.sensor_interrupt = 0
 
-    # Check MQTT brocker
-    async def mqtt_check(self):
+    async def keep_mqtt(self):
         while True:
             await asyncio.sleep(10)
             self.ping_mqtt = time.time()
@@ -215,51 +215,23 @@ class SmartLight():
             # print("Send MQTT ping (%i)" % self.ping_mqtt)
             self.ping_fail += 1
 
-            if self.ping_fail >= config.MQTT_CRIT_ERR:
+            if self.ping_fail > 10:
                 # print("MQTT ping false... reset (%i)" % self.ping_fail)
                 machine.reset()
 
-            if self.ping_fail >= config.MQTT_MAX_ERR:
+            if self.ping_fail > 3:
                 # print("MQTT ping false... reconnect (%i)" % self.ping_fail)
                 self.mqtt_client.disconnect()
                 self.mqtt_connect()
 
-    # Check MQTT message
     async def check_message(self):
         while True:
             self.wdt.feed()
             await asyncio.sleep(0.2)
-            # print("Check message...")
             try:
                 self.mqtt_client.check_msg()
-            except Exception as error:
-                # print("Error in mqtt check message: [Exception] %s: %s" % (type(error).__name__, error))
+            except:
                 self.mqtt_connect()
-
-
-    # Check Internet connected and reconnect
-    async def check_internet(self):
-        try:
-            while True:
-                await asyncio.sleep(60)
-    #            print("Check Internet connect... ")
-                if not self.internet_connected():
-                    # print("Internet connect fail...")
-                    self.int_err_count += 1
-
-                    if self.int_err_count >= config.INT_CRIT_ERR:
-                        self.mqtt_client.disconnect()
-                        wifi.wlan.disconnect()
-                        machine.reset()
-
-                    if self.int_err_count >= config.INT_MAX_ERR:
-                        # print("Internet reconnect")
-                        self.mqtt_client.disconnect()
-                        wifi.wlan.disconnect()
-                        wifi.activate()
-        except Exception as error:
-            pass
-            # print("Error in Internet connection: [Exception] %s: %s" % (type(error).__name__, error))
 
     def run(self):
         self.sound_sensor.irq(trigger=Pin.IRQ_FALLING, handler=self.sensor_action)
@@ -271,8 +243,8 @@ class SmartLight():
         try:
             loop = asyncio.get_event_loop()
             loop.create_task(self.check_message())
-            loop.create_task(self.check_internet())
-            loop.create_task(self.mqtt_check())
+            loop.create_task(self.check_interrupt())
+            loop.create_task(self.keep_mqtt())
             loop.run_forever()
         except Exception as e:
             print(e)
